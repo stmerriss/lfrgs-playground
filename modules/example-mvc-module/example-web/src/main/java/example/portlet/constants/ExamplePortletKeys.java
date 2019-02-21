82ff563d716d9bfd166ab0fd7661178878ae905b
Shane Merriss <shane.merriss@liferay.com>
2019-02-20 17:00:06 -0800

    init example mvc module

---
+++ modules/example-mvc-module/example-web/src/main/java/example/portlet/constants/ExamplePortletKeys.java
@@ -0,0 +1,10 @@
+package example.portlet.constants;
+
+/**
+ * @author liferay
+ */
+public class ExamplePortletKeys {
+
+	public static final String Example = "example";
+
+}
---
+++ modules/example-mvc-module/example-web/src/main/java/example/portlet/portlet/ExamplePortlet.java
@@ -0,0 +1,28 @@
+package example.portlet.portlet;
+
+import example.portlet.constants.ExamplePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ExamplePortletKeys.Example,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ExamplePortlet extends MVCPortlet {
+}
